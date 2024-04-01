140c59d1766d921e5cdbece209c8cedc41257105
Nanguti <g.nanguti@gmail.com>
2024-04-01 12:39:52 +0300

    fix link

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -97,12 +97,13 @@ const Navbar = () => {
                 </Link>
               </li>
               <li>
-                <Link
-                  href="#"
+                <a
+                  href="https://jambohub.decodeafrica.com/"
+                  target="_blank"
                   className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Company
-                </Link>
+                  Jambo Hub
+                </a>
               </li>
               {/* <li>
                 <Link
